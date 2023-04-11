b4083c7fc9f510eec7f01e4b7d96ebdda2f7f897
KTruong9303 <91974329+KTruong9303@users.noreply.github.com>
2023-04-11 22:42:40 +0700

    Update main.py

--- framework/main.py
+++ framework/main.py
@@ -1,4 +1,15 @@
 from src.index import independentFunction
+from src.index import check_triangle, print_Prime
+
 
 if __name__ == "__main__":
+    a, b, c = map(int, input().split())
+    if check_triangle(a,b,c):
+        print("La tam giac hop le")
+    else:
+        print("Khong phai la tam giac")
+    n = int(input())
+    print_Prime(n)
+    
+    
     independentFunction(1)
