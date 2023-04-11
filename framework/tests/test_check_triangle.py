0d8429a59be352f8fc081da876c4ac372dbfdbfc
KTruong9303 <91974329+KTruong9303@users.noreply.github.com>
2023-04-12 00:13:04 +0700

    Update test_check_triangle.py

--- framework/tests/test_check_triangle.py
+++ framework/tests/test_check_triangle.py
@@ -5,7 +5,7 @@ from unittest.mock import patch
 #tamgiac   
 @pytest.mark.parametrize(
     "a, b, c, check_triangle",
-    [(3,3,3, True), (1,1,0, False), (2,1,1, False), (3,4,5, True)],
+    [(3,3,3, True), (1,1,5, False), (1,1,0, False), (2,1,1, False), (3,4,5, True)],
 )
 def test_check_triangle(a, b, c, check_triangle):
     assert check_triangle(a, b, c) == check_triangle
@@ -19,7 +19,9 @@ def mockCheckPrimeNumber(mocker, check_prime_number):
 @pytest.mark.parametrize(
     "n, prpime",
     [
-        (5, 2),
+        (5, 2 3),
+       (2, ),
+       (10, 3 5 7),
         # (4, 2023, False, 30),
         # (2, 2024, True, 29),
         # (3, 2023, False, 31),
